8491dd3e90e8a87f1ce9eec420ef5447fd96cdb0
soloman-err <soloman.err@gmail.com>
2024-02-10 00:04:46 +0600

    Refactor: cleared the demo files from public & lib dirtories

--- src/lib/audio/AudioFiles.js
+++ src/lib/audio/AudioFiles.js
@@ -1,13 +1,4 @@
 const audioFiles = {
-  AF: new Audio("/Afghanistan.mp3"),
-  BD: new Audio("/Bangladesh.mp3"),
-  CN: new Audio("/China.mp3"),
-  IN: new Audio("/India.mp3"),
-  DK: new Audio("/Denmark.mp3"),
-  IR: new Audio("/Iran.mp3"),
-  IQ: new Audio("/Iraq.mp3"),
-  IE: new Audio("/Ireland.mp3"),
-  JP: new Audio("/Japan.mp3"),
   // Add your country-code & audio-file path here
 };
 
