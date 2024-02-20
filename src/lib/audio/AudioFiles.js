e5bb6417b1b8bd7a9f8f762faabd45311f271c09
soloman-err <soloman.err@gmail.com>
2024-02-20 23:24:28 +0600

    feat(anthem): Belarus anthem add along changing the hover state of country path

--- src/lib/audio/AudioFiles.js
+++ src/lib/audio/AudioFiles.js
@@ -1,6 +1,7 @@
 const audioFiles = {
   AF: new Audio("/Afghanistan.mp3"),
   BD: new Audio("/Bangladesh.mp3"),
+  BY: new Audio("/Belarus.mp3"),
   // Add your country-code & audio-file path here
 };
 
